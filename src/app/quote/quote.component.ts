d0a147b001d1aecae498b3d090e3216223a7fde1
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-04-12 14:06:49 +0300

    added the list of quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+quotes:Quote[] =[
+  new Quote(1,"Your journey begins when you take the chance to get up and live fully","Oprah Winfrey","Lona Wanjiku",0,0),
+  new Quote(2,"Don't let the scale define you! Be active Eat healthy,Be Happy","Juice Wrld","Lona Wanjiku",0,0),
+  new Quote(3,"It always seems impossible until it's done","Nelson Mandela","Lona Wanjiku",0,0)
 
+]
+arr:number[] = this.quotes.map(quotes=>quotes.upvotes)
+highest = Math.max(...this.arr)
   constructor() { }
 
   ngOnInit(): void {
